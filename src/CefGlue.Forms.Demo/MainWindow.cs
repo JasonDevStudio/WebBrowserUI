4f93bd820e755363fa5cf511f1914583383beea0
yaojiestuido <yaojiestudio@gmail.com>
2021-01-01 12:39:39 +0800

    CefGlue.Forms.Demo 运行

--- src/CefGlue.Forms.Demo/MainWindow.cs
+++ src/CefGlue.Forms.Demo/MainWindow.cs
@@ -1,20 +1,140 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
-namespace CefGlue.Forms.Demo
+﻿namespace CefGlue.Forms.Demo
 {
+    using System;
+    using System.Drawing;
+    using System.Drawing.Drawing2D;
+    using System.Windows.Forms;
+    using Xilium.CefGlue;
+
+    /// <summary>
+    /// Defines the <see cref="MainWindow" />.
+    /// </summary>
     public partial class MainWindow : Form
     {
+        /// <summary>
+        /// Defines the _handleCreated.
+        /// </summary>
+        private bool _handleCreated;
+
+        /// <summary>
+        /// Defines the _browser.
+        /// </summary>
+        private CefBrowser _browser;
+
+        /// <summary>
+        /// Defines the _browserWindowHandle.
+        /// </summary>
+        private IntPtr _browserWindowHandle;
+
+        /// <summary>
+        /// Gets or sets the StartUrl.
+        /// </summary>
+        public string StartUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets the BrowserSettings.
+        /// </summary>
+        public CefBrowserSettings BrowserSettings { get; set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MainWindow"/> class.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
+
+            SetStyle(
+                ControlStyles.ContainerControl
+                | ControlStyles.ResizeRedraw
+                | ControlStyles.FixedWidth
+                | ControlStyles.FixedHeight
+                | ControlStyles.StandardClick
+                | ControlStyles.UserMouse
+                | ControlStyles.SupportsTransparentBackColor
+                | ControlStyles.StandardDoubleClick
+                | ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.CacheText
+                | ControlStyles.EnableNotifyMessage
+                | ControlStyles.DoubleBuffer
+                | ControlStyles.OptimizedDoubleBuffer
+                | ControlStyles.UseTextForAccessibility
+                | ControlStyles.Opaque,
+                false);
+
+            SetStyle(
+                ControlStyles.UserPaint
+                | ControlStyles.AllPaintingInWmPaint
+                | ControlStyles.Selectable,
+                true);
+
+            this.StartUrl = "http://www.google.com";
+        }
+
+        /// <summary>
+        /// The OnHandleCreated.
+        /// </summary>
+        /// <param name="e">The e<see cref="EventArgs"/>.</param>
+        protected override void OnHandleCreated(EventArgs e)
+        {
+            base.OnHandleCreated(e);
+
+            if (this.DesignMode)
+            {
+                if (!_handleCreated) Paint += PaintInDesignMode;
+            }
+            else
+            {
+                var windowInfo = CefWindowInfo.Create();
+                windowInfo.SetAsChild(Handle, new CefRectangle { X = 0, Y = 0, Width = Width, Height = Height });
+
+                var client = CreateWebClient();
+
+                var settings = BrowserSettings;
+                if (settings == null) settings = new CefBrowserSettings { };
+
+                CefBrowserHost.CreateBrowser(windowInfo, client, settings, StartUrl);
+            }
+
+            _handleCreated = true;
+        }
+
+        /// <summary>
+        /// The PaintInDesignMode.
+        /// </summary>
+        /// <param name="sender">The sender<see cref="object"/>.</param>
+        /// <param name="e">The e<see cref="PaintEventArgs"/>.</param>
+        private void PaintInDesignMode(object sender, PaintEventArgs e)
+        {
+            var width = this.Width;
+            var height = this.Height;
+            if (width > 1 && height > 1)
+            {
+                var brush = new SolidBrush(this.ForeColor);
+                var pen = new Pen(this.ForeColor);
+                pen.DashStyle = DashStyle.Dash;
+
+                e.Graphics.DrawRectangle(pen, 0, 0, width - 1, height - 1);
+
+                var fontHeight = (int)(this.Font.GetHeight(e.Graphics) * 1.25);
+
+                var x = 3;
+                var y = 3;
+
+                e.Graphics.DrawString("CefWebBrowser", Font, brush, x, y + (0 * fontHeight));
+                e.Graphics.DrawString(string.Format("StartUrl: {0}", StartUrl), Font, brush, x, y + (1 * fontHeight));
+
+                brush.Dispose();
+                pen.Dispose();
+            }
+        }
+
+        /// <summary>
+        /// The CreateWebClient.
+        /// </summary>
+        /// <returns>The <see cref="CefWebClient"/>.</returns>
+        protected virtual CefWebClient CreateWebClient()
+        {
+            return new CefWebClient();
         }
     }
 }
--- src/CefGlue.Forms.Demo/Program.cs
+++ src/CefGlue.Forms.Demo/Program.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Xilium.CefGlue;
 
 namespace CefGlue.Forms.Demo
 {
@@ -14,10 +16,39 @@ namespace CefGlue.Forms.Demo
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainWindow());
+            try
+            {
+                Application.SetHighDpiMode(HighDpiMode.SystemAware);
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+
+                CefRuntime.Load(Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), "cef64"));
+
+                var settings = new CefSettings();
+                settings.MultiThreadedMessageLoop = CefRuntime.Platform == CefRuntimePlatform.Windows;
+                settings.LogSeverity = CefLogSeverity.Verbose;
+                settings.LogFile = "cef.log";
+                // settings.ResourcesDirPath = System.IO.Path.GetDirectoryName(new Uri(System.Reflection.Assembly.GetEntryAssembly().CodeBase).LocalPath);
+                settings.RemoteDebuggingPort = 20480;
+                settings.NoSandbox = true;
+
+                var mainArgs = new CefMainArgs(new string[0]);
+                //var exitCode = CefRuntime.ExecuteProcess(mainArgs, app, IntPtr.Zero);
+                //Console.WriteLine("CefRuntime.ExecuteProcess() returns {0}", exitCode);
+                //if (exitCode != -1)
+                //    return exitCode;
+
+                //// guard if something wrong
+                //foreach (var arg in args) { if (arg.StartsWith("--type=")) { return -2; } }
+
+                CefRuntime.Initialize(mainArgs, settings, null, IntPtr.Zero);
+
+                Application.Run(new MainWindow());
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
